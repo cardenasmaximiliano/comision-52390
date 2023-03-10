99e65d0d11babb26aaca3d725ce1a4a706540537
Lautaro Garcia <lautarojgarcia177@gmail.com>
2023-03-10 15:34:18 -0300

    clase 1

---
+++ clase_1__Sintaxis_y_Variables/main.js
@@ -0,0 +1,5 @@
+alert("En este programa se multiplicaran dos números");
+let n1 = prompt("Ingrese el primer número");
+let n2 = prompt("Ingrese el segundo número");
+let R = n1 * n2;
+alert(`el producto de los dos números es: ${R}`);
